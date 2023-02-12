3b1f4ddf0604143b9cb325a0b686479d7149189f
LuKirill <98026813+LuKirill@users.noreply.github.com>
2023-02-12 23:57:19 +0400

    Lesson 2
    Lesson 2

---
+++ TODO/frontend/src/components/Footers.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Footer = () => {
+
+    return(
+    <b>
+    ________________________________________________
+    </b>
+    );
+};
+
+
+export default Footer;
