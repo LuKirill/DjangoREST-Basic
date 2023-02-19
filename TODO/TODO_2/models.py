9ec054e033749807f0a6a48ea789318a4360d23e
Cyril <kirill-mihalich@mail.ru>
2023-02-19 21:48:54 +0300

    models for project

--- TODO/TODO_2/models.py
+++ TODO/TODO_2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.model):
+    name = models.CharField(max_length=32)
+    repo_link = models.URLField()
+
+class Users(models.Model):
+    project = models.OneToOneField(Project, related_name='user_set', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=32)
+    second_name = models.CharField(max_length=32)
+    email = models.EmailField()
+
