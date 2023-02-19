2dfb135b3f9e266bac85eb0e853482505e29bb41
Cyril <kirill-mihalich@mail.ru>
2023-02-19 22:07:33 +0300

    Model TODO

--- TODO/TODO_2/models.py
+++ TODO/TODO_2/models.py
@@ -10,3 +10,12 @@ class Users(models.Model):
     second_name = models.CharField(max_length=32)
     email = models.EmailField()
 
+class TODO(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    date = models.DateField()
+    author = models.OneToOneField(Users, on_delete=models.CASCADE)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.title
