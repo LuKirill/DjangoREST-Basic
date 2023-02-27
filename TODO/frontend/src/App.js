a7e1d2e8cc4ed0856230a9f85553ae73f8471952
Cyril <kirill-mihalich@mail.ru>
2023-02-27 23:15:23 +0300

    npm install react-router-dom

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,54 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import UserList from './components/User.js';
 import axios from 'axios';
-import Menu from './components/Menu.js';
-import Footer from './components/Footers.js';
+//import logo from './logo.svg';
+import './App.css';
+//import Custom_users_list from './components/custom_users.js';
+//import Footer from './components/Footer.js';
+//import Menu from './components/Menu.js';
+import UserList from './components/User.js';
+import ProjectList from './components/Project.js';
+import ToDoList from './components/ToDo.js';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 
+const NotFound404 = ({ location }) => {
+    return (
+        <div>
+            <h1>Страница по адресу '{location.pathname}' не найдена</h1>
+        </div>
+    )
+}
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      'users': []
-    };
-  }
+    constructor(props) {
+        super(props);
+        const user1 = {id: 1, first_name: 'User1', last_name: 'UserLastName1', email: 'user1@mail.ru', project: '1'}
+        const users = [user1]
+        const project1 = {id: 1, name: 'Project1', repo_link: 'http://repo1.ru'}
+        const projects = [project1]
+        const todo1 = {id: 1, title: 'Title1', body: 'Lorem1', author: '1'}
+        const TODO_2 = [todo1]
 
-  componentDidMount() {
-    // const users = [
-    //   {
-    //     'first_name': 'Фёдор',
-    //     'last_name': 'Достоевский',
-    //     'birthday_year': 1821
-    //   },
-    //   {
-    //     'first_name': 'Александр',
-    //     'last_name': 'Грин',
-    //     'birthday_year': 1880
-    //   },
-    // ]
-    axios.get('http://127.0.0.1:8000/api/users/')
-      .then(response => {
-        // const users = response.data
-        this.setState(
-          {
-            'users': response.data.results
-          }
+        this.state = {
+        'users': users,
+        'projects': projects,
+        'TODO_2': TODO_2
+        }
+    }
+
+    componentDidMount() {
+
+        axios.get('http://127.0.0.1:8000/api/user_set/')
+            .then(response => {
+
+                    this.setState({users: response.data})
+            }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/Project/')
+            .then(response => {
+
+                    this.setState({projects: response.data})
+            }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/TODO/')
+            .then(response => {
+
+                    this.setState({todos: response.data})
+            }).catch(error => console.log(error))
+    }
+
+    render () {
+        return (
+            <div className='App'>
+                <BrowserRouter>
+                <nav>
+                    <ul>
+                        <li>
+                           <Link to='/'>Users</Link>
+                        </li>
+                        <li>
+                           <Link to='/projects'>Projects</Link>
+                        </li>
+                        <li>
+                           <Link to='/TODO'>ToDos</Link>
+                        </li>
+                    </ul>
+                </nav>
+                    <Switch>
+                        <Route exact path='/' component={() => <UserList
+                        users={this.state.users} />} />
+                        <Route exact path='/projects' component={() => <ProjectList
+                        projects={this.state.projects} />} />
+                        <Route exact path='/TODO' component={() => <ToDoList
+                        projects={this.state.TODO_2} />} />
+                        <Route component={NotFound404} />
+                    </Switch>
+                </BrowserRouter>
+            </div>
         )
-      }).catch(error => console.log(error))
-  }
-
-  render() {
-    return (
-      <div>
-        <Menu/>
-        <UserList users={this.state.users} />
-        <Footer/>
-      </div>
-    );
-  }
-}
+        }
+    }
 
 export default App;
--- TODO/frontend/src/components/User.js
+++ TODO/frontend/src/components/User.js
@@ -1,21 +1,14 @@
-import React from 'react';
+import React from 'react'
 
 
 const UserItem = ({user}) => {
     return (
         <tr>
-            <td>
-                {user.username}
-            </td>
-            <td>
-                {user.firstname}
-            </td>
-            <td>
-                {user.lastname}
-            </td>
-            <td>
-                {user.email}
-            </td>
+            <td>{user.id}</td>
+            <td>{user.first_name}</td>
+            <td>{user.last_name}</td>
+            <td>{user.email}</td>
+            <td>{user.project}</td>
         </tr>
     )
 }
@@ -23,22 +16,16 @@ const UserItem = ({user}) => {
 const UserList = ({users}) => {
     return (
         <table>
-            <th>
-                User name
-            </th>
-            <th>
-                First name
-            </th>
-            <th>
-                Last name
-            </th>
-            <th>
-                Email
-            </th>
+            <tr>
+                <th>ID</th>
+                <th>FIRST_NAME</th>
+                <th>LAST_NAME</th>
+                <th>EMAIL</th>
+                <th>PROJECT</th>
+            </tr>
             {users.map((user) => <UserItem user={user} />)}
         </table>
     )
 }
-
 
 export default UserList
