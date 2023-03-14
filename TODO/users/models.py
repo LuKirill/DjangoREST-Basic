a9d9c002e9f1a66d7ef62457f824232f0a322719
LuKirill <98026813+LuKirill@users.noreply.github.com>
2023-03-14 23:34:02 +0300

    Lesson 9 (#17)
    lesson_9

--- TODO/users/models.py
+++ TODO/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class CustomUser(AbstractUser):
