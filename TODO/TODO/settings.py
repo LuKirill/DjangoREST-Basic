4c29381f259f462abc7268bb23b2fb0f07df605d
Cyril <kirill-mihalich@mail.ru>
2023-03-20 22:34:59 +0300

    docker

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -95,6 +95,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': '123',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
